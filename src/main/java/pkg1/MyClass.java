2676194c77bb96583c444ae9b777c298ec7c834c
pragya <500090986@stu.upes.ac.in>
2023-02-18 20:33:46 +0530

    first commit

---
+++ src/main/java/pkg1/MyClass.java
@@ -0,0 +1,17 @@
+package pkg1;
+public class MyClass{
+		public void show()
+		{
+			System.out.println("Hello Devops! Welcome to Jenkis");
+		}
+
+
+
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		ob.show();
+		
+
+	}
+
+}
